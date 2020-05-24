6c6a8815c0b95a1415c0bb3f24c30b8ea0610ecc
Samuel Cardoso <scksamuel.sc@gmail.com>
2020-05-24 17:27:57 -0300

    feat(feed): recebe stories usando fetch

--- src/routes/FeedRoute/FeedRoute.jsx
+++ src/routes/FeedRoute/FeedRoute.jsx
@@ -8,8 +8,29 @@ import Posts from '../../containers/Posts';
 import './FeedRoute.scss';
 
 const FeedRoute = () => {
+  const [users, setUsers] = useState([]);
+  const [stories, setStories] = useState([]);
+
+  // Get users
+  useEffect(() => {
+    fetch('https://5e7d0266a917d70016684219.mockapi.io/api/v1/users')
+      .then((res) => res.json())
+      .then(data => { setUsers(data); console.log(data); });
+  }, []);
+
+  // Get stories
+  useEffect(() => {
+    fetch('https://5e7d0266a917d70016684219.mockapi.io/api/v1/stories')
+      .then((res) => res.json())
+      .then(data => {
+        setStories(data);
+      });
+  }, [users]);
+
+
   return (
     <div>
+      <Stories stories={stories} getUserHandler={(userId) => users.find(user => userId === user.id)} />
     </div>
   );
 };

76ddb216031f80fd8948761476d43d579a8473fd
Samuel Cardoso <scksamuel.sc@gmail.com>
2020-05-25 00:50:01 -0300

    feat(user posts): implementa container de posts do usuário

--- src/containers/UserPosts/UserPosts.jsx
+++ src/containers/UserPosts/UserPosts.jsx
@@ -6,7 +6,26 @@ import './UserPosts.scss';
 
 const UserPosts = ({ posts }) => (
   <div className="container">
-  </div>
+    {posts?.length > 0 ?
+      (<section className="user-posts">
+        {posts.map(post => (
+          <article key={post.id} className="post">
+            <img className="post__figure" src={post.imageUrl} alt={post.id} />
+          </article>
+        ))
+        }
+      </section>)
+      :
+      (<div className="no-posts">
+        <span className="no-posts__content">
+          Este usuário ainda não publicou nada
+        </span>
+        <span className="no-posts__emoji">
+          🙁
+        </span>
+      </div>)
+    }
+  </div >
 );
 
 export default UserPosts;

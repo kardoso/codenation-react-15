c2a419ac90b5bab5700f24a53166dd0994ea5c21
Samuel Cardoso <scksamuel.sc@gmail.com>
2020-05-25 00:18:43 -0300

    feat(profile route): implementa rota para requisições do perfil do usuário

--- src/routes/ProfileRoute/ProfileRoute.jsx
+++ src/routes/ProfileRoute/ProfileRoute.jsx
@@ -5,9 +5,35 @@ import UserPosts from '../../containers/UserPosts';
 
 import Loading from '../../components/Loading';
 
-const ProfileRoute = () => {
+const ProfileRoute = (props) => {
+  const [user, setUser] = useState(null);
+  const [posts, setPosts] = useState(null);
+
+  useEffect(() => {
+    let username = (props.match.params.username);
+    fetch(`https://5e7d0266a917d70016684219.mockapi.io/api/v1/users?search=${username}`)
+      .then(res => res.json())
+      .then(data => setUser(data[0]));
+  }, []);
+
+  useEffect(() => {
+    user &&
+      fetch(`https://5e7d0266a917d70016684219.mockapi.io/api/v1/users/${user.id}/posts`)
+        .then(res => res.json())
+        .then(data => setPosts(data))
+        .catch(err => console.log(err));
+  }, [user]);
+
   return (
     <div>
+      {user ? (
+        <div>
+          <UserProfile avatar={user.avatar} name={user.name} username={user.username} />
+          <UserPosts posts={posts} />
+        </div>
+      ) :
+        (<Loading />)
+      }
     </div>
   );
 };

d0642803ef1bf3bd4ecb24a5001aad6bd735940a
Samuel Cardoso <scksamuel.sc@gmail.com>
2020-05-24 22:52:02 -0300

    feat(users list): implementa listagem de usuários

--- src/containers/UsersList/UsersList.jsx
+++ src/containers/UsersList/UsersList.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 import User from '../../components/User';
 import Loading from '../../components/Loading';
@@ -8,8 +9,22 @@ import './UsersList.scss';
 const UersList = ({ users }) => {
   return (
     <section className="users-list">
+      {users?.map(user => (
+        <article className="post">
+          <header className="post__header">
+            <Link to={`/user/${user.id}`} className="user">
+              <div className="user__thumb">
+                <img src={user.avatar} />
+              </div>
+              <div className="user__name">
+                {user.name}
+              </div>
+            </Link>
+          </header>
+        </article>
+      ))}
     </section>
-  )
+  );
 };
 
 export default UersList;

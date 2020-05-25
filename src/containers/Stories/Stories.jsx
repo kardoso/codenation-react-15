5e67d5c2298cb27ed210c238e8d49fe1a110bd87
Samuel Cardoso <scksamuel.sc@gmail.com>
2020-05-24 21:46:22 -0300

    feat(stories): exibe stories no feed

--- src/containers/Stories/Stories.jsx
+++ src/containers/Stories/Stories.jsx
@@ -1,21 +1,56 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 import Story from '../../components/Story';
 
 import './Stories.scss';
 
 const Stories = ({ stories, getUserHandler }) => {
-  const [showStory, setShowStory] = useState(false);
+  const [showStory, toggleShowStory] = useState(false);
+  const [selectedStory, setSelectedHistory] = useState({});
+  const [selectedProfile, setSelectedProfile] = useState({});
+
+  const handleOpenStory = (story) => {
+    const foundStory = stories.find(foundStory => foundStory.id === story.id);
+    const profileData = getUserHandler(story.userId);
+
+    setSelectedProfile(profileData);
+    setSelectedHistory(foundStory);
+    toggleShowStory(!showStory);
+  };
+
+  const handleCloseStory = () => {
+    toggleShowStory(!showStory);
+  };
 
   return (
     <React.Fragment>
       <section className="stories">
         <div className="container">
+          {
+            stories.map(story => {
+              const profileData = getUserHandler(story.userId);
+
+              return (
+                <button
+                  key={story.id}
+                  className={`user__thumb ${story.id == 1 && 'user__thumb--hasNew'}`}
+                  onClick={() => handleOpenStory(story)}
+                >
+                  <div className="user__thumb__wrapper">
+                    <img src={profileData?.avatar} alt={profileData?.name} />
+                  </div>
+                </button>
+              );
+            })}
         </div>
       </section>
 
       {showStory && (
-        <Story />
+        <Story
+          story={selectedStory}
+          user={selectedProfile}
+          handleClose={handleCloseStory}
+        />
       )}
     </React.Fragment>
   );

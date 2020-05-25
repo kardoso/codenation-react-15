8b501e484bf236ab93152bdbdbc3e6956f40b40b
Samuel Cardoso <scksamuel.sc@gmail.com>
2020-05-25 00:19:25 -0300

    feat(user profile): implementa componente de perfil do usuário

--- src/containers/UserProfile/UserProfile.jsx
+++ src/containers/UserProfile/UserProfile.jsx
@@ -5,8 +5,16 @@ import './UserProfile.scss';
 const UserProfile = ({ avatar, name, username }) => {
   return (
     <section className="profile">
+      <article className="profile-data">
+        <div className="user">
+          <img className="user__thumb" src={avatar} alt={username} />
+          <p className="user__name">
+            {name}<span>@{username}</span>
+          </p>
+        </div>
+      </article>
     </section>
-  )
+  );
 };
 
 export default UserProfile;

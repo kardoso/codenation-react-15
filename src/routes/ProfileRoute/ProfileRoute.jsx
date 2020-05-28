7d914462d74ff216c8dbf22b70f4af0590e178b0
Samuel Cardoso <scksamuel.sc@gmail.com>
2020-05-28 11:34:51 -0300

    fix(user profile): condiciona parâmetro ao buscar usuário

--- src/routes/ProfileRoute/ProfileRoute.jsx
+++ src/routes/ProfileRoute/ProfileRoute.jsx
@@ -1,38 +1,46 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 
-import UserProfile from '../../containers/UserProfile';
-import UserPosts from '../../containers/UserPosts';
+import UserProfile from "../../containers/UserProfile";
+import UserPosts from "../../containers/UserPosts";
 
-import Loading from '../../components/Loading';
+import Loading from "../../components/Loading";
 
 const ProfileRoute = (props) => {
   const [user, setUser] = useState(null);
   const [posts, setPosts] = useState(null);
 
   useEffect(() => {
-    fetch(`https://5e7d0266a917d70016684219.mockapi.io/api/v1/users?search=${props.match.params.username}`)
-      .then(res => res.json())
-      .then(data => setUser(data[0]));
-  }, [props.match.params.username]);
+    fetch(
+      `https://5e7d0266a917d70016684219.mockapi.io/api/v1/users?search=${props.match?.params.username}`
+    )
+      .then((res) => res.json())
+      .then((data) => setUser(data[0]));
+  }, [props.match?.params.username]);
 
   useEffect(() => {
     user &&
-      fetch(`https://5e7d0266a917d70016684219.mockapi.io/api/v1/users/${user.id}/posts`)
-        .then(res => res.json())
-        .then(data => setPosts(data))
-        .catch(err => console.log(err));
+      fetch(
+        `https://5e7d0266a917d70016684219.mockapi.io/api/v1/users/${user.id}/posts`
+      )
+        .then((res) => res.json())
+        .then((data) => setPosts(data))
+        .catch((err) => console.log(err));
   }, [user]);
 
   return (
     <div data-testid="profile-route">
       {user ? (
         <div>
-          <UserProfile avatar={user.avatar} name={user.name} username={user.username} />
+          <UserProfile
+            avatar={user.avatar}
+            name={user.name}
+            username={user.username}
+          />
           <UserPosts posts={posts} />
         </div>
-      ) :
-        (<Loading />)
-      }
+      ) : (
+        <Loading />
+      )}
     </div>
   );
 };
